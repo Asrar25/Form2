3d70a50679fbd994bebda5db2ab0d40857c33616
Asrar A <114174920+Asrar25@users.noreply.github.com>
2024-07-11 18:44:08 +0530

    Completed

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './index.css';
+
+import FormValidation from './components/FormValidation';
+
+function App() {
+  return (
+   <div>
+          <FormValidation />
+  </div>
+  );
+}
+
+export default App;
